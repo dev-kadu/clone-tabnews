41c0c1e6fcba9fe10a60a2ac192db1dd40dccb11
dev-kadu <dev-kadu@github.com>
2026-02-07 05:43:27 -0300

    debug type error in

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NOCE_ENV === "development" ? false : true,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  console.log("Credendciais do Postgres: ", {
+  console.log("Credenciais do Postgres: ", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
