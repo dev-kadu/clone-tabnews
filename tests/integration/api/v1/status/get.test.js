f89aa46d211cf81751c9d485437e7481a4cbaf28
dev-kadu <dev-kadu@github.com>
2026-02-09 10:06:18 -0300

    add  end point

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,9 @@
 test("Get to /api/v1/status returns status 200 and body {status: 'ok'}", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-
   const responseBody = await response.json();
-  console.log(responseBody);
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
   expect(responseBody.dependencies.database.version).toEqual("18.1");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
